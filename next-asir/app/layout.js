b914d3f65af7c8f270d33393cc4b1bacc66ff781
FabioFabrega <fabiofabrega746@gmail.com>
2024-12-06 18:07:28 +0100

    cambios

--- next-asir/app/layout.js
+++ next-asir/app/layout.js
@@ -1,17 +1,9 @@
 'use client'
 import "./globals.css";
-import { useState } from "react";
-import { getDictionary } from "@/componentes/diccionario";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "@/componentes/Nav";
 
 function RootLayout({ setGeneration }) {
-  let [idioma, setIdioma] = useState('en');
-  let dict = getDictionary(idioma);
-
-  const changeLanguage = (lang) => {
-    setIdioma(lang);
-  }
   return (
     <html lang="en">
       <body>
