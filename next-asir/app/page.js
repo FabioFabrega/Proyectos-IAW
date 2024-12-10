ff29bf0f870608e95d631824e033dbe163bd51d7
FabioFabrega <fabiofabrega746@gmail.com>
2024-12-10 12:02:54 +0100

    ces

--- next-asir/app/page.js
+++ next-asir/app/page.js
@@ -1,12 +1,82 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>
-      <img src="/header.jpg" alt="un ejemplo de imagen" ></img>
-    </div>
-  )
-}
+'use client'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import React, { useState, useEffect } from 'react';
+const Pokemon2 = ({ id }) => {
+    const [pokemon, setName] = useState([]);
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    useEffect(() => {
+      const idAleatorio = Math.floor(Math.random() * 898) + 1;
+      fetch(`https://pokeapi.co/api/v2/pokemon/${idAleatorio}`)
+        .then((res) => res.json())
+        .then((data) =>
+          setName({
+            numero: data.id,
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+            imgJuego: data.sprites.front_default,
+            imgCvg: data.sprites.other.dream_world.front_default,
+            nombre: data.name,
+            experiencia: data.base_experience,
+            hp: data.stats[0].base_stat,
+            ataque: data.stats[1].base_stat,
+            defensa: data.stats[2].base_stat,
+            especial: data.stats[3].base_stat,
+          })
+        );
+    }, []);
+  
+    return (
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <img src={pokemon.img} alt="pokemon" className="pokemon-image" />
+          <h1>
+            <p className="pokemon-nombre">{pokemon.nombre}:</p>
+          </h1>
+          <h2>
+            <p className="pokemon-numero">Número {pokemon.numero}</p>
+          </h2>
+          <h4>
+            <p className="extra">
+              <Button variant="primary" onClick={handleShow}>
+                Saber más
+              </Button>
+            </p>
+          </h4>
+        </Card>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Número {pokemon.numero} {pokemon.nombre}:
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              <img src={pokemon.img} alt="pokemon" className="pokemon-image" />
+            </p>
+            <h2>
+              <p>
+                HP: {pokemon.hp} Ataque: {pokemon.ataque}
+              </p>
+              <p>
+                Defensa: {pokemon.defensa} Especial: {pokemon.especial}
+              </p>
+            </h2>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Salir
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  };
+  
+  export default Pokemon2;
+  
